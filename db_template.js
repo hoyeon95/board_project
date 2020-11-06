32d1e4ed4002f4bae758fabd952e025dca9054f1
unknown <you@example.com>
2020-11-06 15:36:00 +0900

    1106

---
+++ db_template.js
@@ -0,0 +1,54 @@
+const mysql = require("mysql")
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'',
+    password:'',
+    port:3306,
+    database:'node_db'
+});
+
+function getAllMemos(callback){
+    connection.query(`SELECT * FROM MEMOS ORDER BY ID DESC`, (err, rows, fields) => {
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
+function insertMemo(content, callback){
+    connection.query(`INSERT INTO MEMOS (CONTENT, CREATED_AT, UPDATED_AT) VALUES ('${content}',NOW(),NOW())`, (err, result) => {
+        if(err) throw err;
+        callback();
+    });
+}
+
+function getMemoById(id, callback){
+    connection.query(`SELECT * FROM MEMOS WHERE ID = ${id}`, (err, row, fields)=>{
+        if(err) throw err;
+        callback(row);
+    });
+}
+
+function updateMemoById(id, content, callback){
+    console.log('id : ',id)
+    console.log('content : ', content)
+    connection.query(`UPDATE MEMOS SET CONTENT='${content}', UPDATED_AT=NOW() WHERE ID=${id}`, (err, result) => {
+        if(err) throw err;
+        callback();
+    });
+}
+
+function deleteMemoById(id, callback){
+    connection.query(`DELETE FROM MEMOS WHERE ID=${id}`, (err, result) => {
+        if(err) throw err;
+        callback();
+    });
+}
+
+module.exports = {
+    getAllMemos,
+    insertMemo,
+    getMemoById,
+    updateMemoById,
+    deleteMemoById,
+}
